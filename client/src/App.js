a5524b399ca51eac34199a4fc9429497990378b3
hickeymeng <daxiam@hickeymengdeMacBook-Pro.local>
2024-05-29 19:26:09 +0800

    重新更新 更新到搭建后端

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./compnents/Footer";
+import Header from "./compnents/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
   return (
-    <>
-      <h1>头号玩家商城</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/products/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
